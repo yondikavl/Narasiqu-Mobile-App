3175802242f11b8bfe3ff541de438fd9bffa6f3b
yondikavl <yondikaviolanda@gmail.com>
2024-05-23 19:21:37 +0700

    fix: map

--- app/src/main/java/com/yondikavl/narasiqu/MapsActivity.kt
+++ app/src/main/java/com/yondikavl/narasiqu/MapsActivity.kt
@@ -1,8 +1,14 @@
 package com.yondikavl.narasiqu
 
+import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.yondikavl.narasiqu.databinding.ActivityMapsBinding
 import com.yondikavl.narasiqu.viewModels.MapStoryModels
 import com.yondikavl.narasiqu.viewModels.ViewModelsFactory
@@ -10,8 +16,10 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -28,7 +36,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         bind = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(bind.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -42,22 +49,69 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.uiSettings.isCompassEnabled = true
         mMap.uiSettings.isMapToolbarEnabled = true
 
-        val local = LatLng(-6.137962924705028, 106.78188193263647)
-        mMap.addMarker(MarkerOptions().position(local).title("mantap"))
-        boundsBuilder.include(local)
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(local))
+        val lampung = LatLng(-5.562261, 105.547437)
+        mMap.addMarker(MarkerOptions().position(lampung).title("Marker in Lampung").snippet("Lampung Selatan"))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lampung, 15f))
+//        callMapFromApi(mMap)
 
-        callMapFromApi(mMap)
+        mMap.setOnPoiClickListener { pointOfInterest ->
+            val poiMarker = mMap.addMarker(
+                MarkerOptions()
+                    .position(pointOfInterest.latLng)
+                    .title(pointOfInterest.name)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
+            )
+            poiMarker?.showInfoWindow()
+        }
+
+        getMyLocation()
+        setMapStyle()
     }
 
-    private fun callMapFromApi(mMap: GoogleMap) {
-        mapStoryModel.getStory().observe(this){
-            it.forEach { place ->
-                val latLng = LatLng(place.lat!!, place.lon!!)
-                mMap.addMarker(MarkerOptions().position(latLng).title(place.name))
-                boundsBuilder.include(latLng)
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
-            }
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            getMyLocation()
         }
     }
+
+    private fun getMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun setMapStyle() {
+        try {
+            val success =
+                mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (exception: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", exception)
+        }
+    }
+
+    companion object {
+        private const val TAG = "MapsActivity"
+    }
+
+//    private fun callMapFromApi(mMap: GoogleMap) {
+//        mapStoryModel.getStory().observe(this){
+//            it.forEach { place ->
+//                val latLng = LatLng(place.lat!!, place.lon!!)
+//                mMap.addMarker(MarkerOptions().position(latLng).title(place.name))
+//                boundsBuilder.include(latLng)
+//                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+//            }
+//        }
+//    }
 }
